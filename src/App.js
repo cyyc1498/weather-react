9973629a4bc0f662a15991a4881feeb464df487e
cyyc1498 <78880455+cyyc1498@users.noreply.github.com>
2022-05-21 15:56:37 -0400

    fixed default page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,35 @@ export default function App() {
         <button id="current-location">
           <i className="fa-solid fa-location-crosshairs"></i>
         </button>
-        <SearchEngine/>
+        <SearchEngine />
+        <div className="current">
+          <h1 id="city"> Toronto </h1>
+          <h2 id="currentTemp">
+            <span id="temp">20</span>
+            <span className="unit">°C</span>
+          </h2>
+          <div className="row row_height">
+            <div className="col-6">
+              <i class="fa-solid fa-sun" id="icon"></i>
+            </div>
+            <div className="col-6 midright">
+              <ul className="rainWind">
+                <li>
+                  <span id="description"></span>
+                </li>
+                <li>
+                  Wind: <span id="wind">20</span>km/hr
+                </li>
+                <li>
+                  Feels like: <span id="feelsLike">20</span>
+                  <span className="unit">°C</span>
+                </li>
+              </ul>
+              <br />
+            </div>
+          </div>
+        </div>
+
         <hr />
         <div id="forecast" className="row">
           <Forecast />
